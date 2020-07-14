afe7a908479382a07a362dd6219fe400111ee0f3
ritsource <ritwiksahaofficial@gmail.com>
2020-07-15 04:45:10 +0530

    Enhancements!

--- prototype/scripts/run-server-meta.sh
+++ prototype/scripts/run-server-meta.sh
@@ -5,11 +5,11 @@ BIN_PATH="$(git rev-parse --show-toplevel)/prototype/.out/server.out"
 function stop_server() {
     PID=$(ps -ef | grep -v grep | grep $BIN_PATH | awk '{print $2}')
     kill $PID >>/dev/null
-    # printf "\033[1;33mServer stopped 😃!\033[0m\n"
 }
 
 # source - https://stackoverflow.com/questions/360201/how-do-i-kill-background-processes-jobs-when-my-shell-script-exits
 trap "stop_server" EXIT
+
 # trap "stop_server; printf \"\033[1;33mServer stopped 😃!\033[0m\n\"; exit" 2
 # trap "stop_server; echo \"Hello world\"" EXIT
 # trap "stop_server" SIGINT SIGTERM EXIT
@@ -18,6 +18,12 @@ trap "stop_server" EXIT
 # there's no good in trigger a compile while
 # there's alread a compilation going on
 export BUILD_IN_PROCESS=0
+
+# while compiling `run-server.sh` ignore all
+# the file change events. this variable tells
+# the script if there has been a latest change
+# is the source code, so it can recompile once
+# the current compilation is done
 export THERE_EXISTS_A_NEWER_CHANGE=0
 
 # source - https://stackoverflow.com/a/38229197/9406420
--- prototype/server/main.go
+++ prototype/server/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/ritsource/episteme/prototype/data/models"
-	"github.com/ritsource/episteme/prototype/server/renderers"
 	"github.com/ritsource/episteme/prototype/server/repo"
 )
 
@@ -24,7 +23,7 @@ func main() {
 	// 	})
 	// })
 
-	r.GET("/", renderers.RootHandler)
+	// r.GET("/", renderers.RootHandler)
 
 	r.Run()
 }
